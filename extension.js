46a3b97baf7d3fa166ec711f76a3e8414f0e7ecc
Thomas Rich <tnrich@teselagen.com>
2018-08-22 11:34:14 -0700

    adding functionality

--- extension.js
+++ extension.js
@@ -5,19 +5,24 @@ const vscode = require('vscode');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "remove-whitespace-aka-join-words" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+    let disposable = vscode.commands.registerCommand('remove_whitespace.removeWhitespaceAkaJoinWords', function () {
         // The code you place here will be executed every time your command is executed
-
+        // vscode.window.activeTextEditor.selection
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        const editor = vscode.window.activeTextEditor
+        
+        const text = editor.document.getText(editor.selection)
+        const newText = text.replace(/\s/g, "");
+        editor.edit((editBuilder) => {
+            editBuilder.replace(editor.selection, newText)
+        })
+
     });
 
     context.subscriptions.push(disposable);
